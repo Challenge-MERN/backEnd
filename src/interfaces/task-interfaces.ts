963eba4a4bbf7cf202e2767ac3238fe4801e0300
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-06-28 20:30:03 -0600

    Task interface implemented

---
+++ src/interfaces/task-interfaces.ts
@@ -0,0 +1,11 @@
+
+
+export interface TaskI {
+    Task_Name: string,
+    Description: string,
+    Date: string,
+    Date_Completion: string,
+    Importance: string,
+    Status: boolean,
+    User_Name: string
+}
