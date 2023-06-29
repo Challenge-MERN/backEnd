9acc2d794e2c78b85851ac0bbb466fa8a8b1e0f2
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-06-28 23:56:57 -0600

    Auth user result constant created

--- src/const/const.ts
+++ src/const/const.ts
@@ -2,3 +2,9 @@ export const TASK_STATUS = {
     FINISH: true,
     START: false,
 }
+
+export const AUTH_USER_RESULT = {
+    SUCCESS: 'Success',
+    FAILED: 'Failed',
+    NOT_FOUND: 'Not found',
+}
