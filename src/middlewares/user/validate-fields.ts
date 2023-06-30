172d62913cf6783122c858456543ca449fdeb413
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-06-29 22:25:57 -0600

    Implemented user middlewares

---
+++ src/middlewares/user/validate-fields.ts
@@ -0,0 +1,43 @@
+import { validationResult, check, param } from 'express-validator';
+import { Request, Response, NextFunction } from 'express';
+
+// Rules
+export const createUserRules = () => {
+    return [
+        check('UserName', 'UserName es obligatorio').not().isEmpty(),
+        check('Mail', 'Correo no válido').isEmail(),
+        check('Password', 'El Password es obligatorio').not().isEmpty(),
+        check('Password', 'El Password debe de ser mas de 5 letas').isLength({ min: 5 }),
+    ];
+}
+
+export const authUserRules = () => {
+    return [
+        check('UserName', 'UserName es obligatorio').not().isEmpty(),
+        check('Password', 'El Password es obligatorio').not().isEmpty(),
+        check('Password', 'El Password debe de ser mas de 5 letas').isLength({ min: 5 }),
+    ];
+}
+
+export const userParams = () => {
+    return [
+        param('UserName', 'UserName es obligatorio').not().isEmpty(),
+    ];
+}
+
+// Validations
+export const validateFields = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        return next();
+    }
+    const extractedErrors: any[] = [];
+    errors.array().map((err: any) => extractedErrors.push({ [err.path]: err.msg }));
+
+    res.status(422).json({
+        status: 'FAILED',
+        data: extractedErrors,
+    });
+}
+
+
